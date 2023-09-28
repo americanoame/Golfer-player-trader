501a0498932ef935ee488d88c1b4acebbb8112dd
Americano.Dev <andrenations@gmail.com>
2023-09-27 20:19:36 -0400

    organizied the names for the routes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import { signup, signing, google, signout } from '../controllers/auth.controller.js';
+import { signup, signing, google, signOut, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
+
 
 
 const router = express.Router();
 
-router.post('/signup', signup)
-router.post('/signing', signing)
+router.post('/signup', signup);
+router.post('/signing', signing);
 router.post('/google', google);
-router.get('/signout', signout)
+router.get('/sign-out', signOut);
+
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+
 
 export default router; 
