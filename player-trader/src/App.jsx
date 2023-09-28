8b77ae89d9f4df4361e94ce7af147b433bc16315
Americano.Dev <andrenations@gmail.com>
2023-09-27 20:20:22 -0400

    added the forgot and reset password routes

--- player-trader/src/App.jsx
+++ player-trader/src/App.jsx
@@ -5,8 +5,8 @@ import Signup from './pages/Signup';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-
-
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 
 function App() {
@@ -18,6 +18,8 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       
       <Route path="/" element={<Home />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
       
       <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
